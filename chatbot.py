21c6636b5c986a9000f62ad9565e96b0778475b4
rigtee <tom.rigal@outlook.com>
2021-05-15 15:32:08 +0200

    ELIZA chatbot finalization: matching rule function (checks if statement is contained in the dictionary) and replace pronouns function (replaces I with you and me with your e.g.) to simulate natural language

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 import json
 import random
+import re
 
 with open('intents.json') as file:
     intents = json.load(file)
@@ -7,33 +8,80 @@ with open('intents.json') as file:
 bot_template = "BOT: {0}"
 user_template = "USER: {0}"
 
-name = "Marius"
-weather = "sunny"
-
 responses = {
-  "what's your name?": [
-      "my name is {0}".format(name),
-      "they call me {0}".format(name),
-      "I go by {0}".format(name)
-   ],
-  "what's today's weather?": [
-      "the weather is {0}".format(weather),
-      "it's {0} today".format(weather)
+    'question': [
+        "I don't know :(", 'you tell me!'
     ],
-  "default": ["default message"]
+    'statement': [
+        'tell me more!',
+        'why do you think that?',
+        'how long have you felt this way?',
+        'I find that extremely interesting',
+        'can you back that up?',
+        'oh wow!',
+        ':)'
+    ],
+    'default': 'default message'
 }
+
+rules = {'I want (.*)': ['What would it mean if you got {0}',
+                         'Why do you want {0}',
+                         "What's stopping you from getting {0}"],
+         'do you remember (.*)': ['Did you think I would forget {0}',
+                                  "Why haven't you been able to forget {0}",
+                                  'What about {0}',
+                                  'Yes .. and?'],
+         'do you think (.*)': ['if {0}? Absolutely.', 'No chance'],
+         'if (.*)': ["Do you really think it's likely that {0}",
+                     'Do you wish that {0}',
+                     'What do you think about {0}',
+                     'Really--if {0}']}
+
 
 def send_message(message):
     print(user_template.format(message))
     response = respond(message)
     print(bot_template.format(response))
 
-def respond(message):
-    if message in responses:
-        bot_message = random.choice(responses[message])
-    else:
-        bot_message = random.choice(responses["default"])
-    return bot_message
 
-send_message("what's your name?")
-send_message("what's today's weather?")
+def respond(message):
+    response, phrase = match_rule(rules, message)
+    if '{0}' in response:
+        phrase = replace_pronouns(phrase)
+        response = response.format(phrase)
+    return response
+
+def match_rule(rules, message):
+    response, phrase = "default", None
+    for pattern, responses in rules.items():
+        match = re.search(pattern, message)
+        if match is not None:
+            response = random.choice(responses)
+            if '{0}' in response:
+                phrase = match.group(1)
+    return response, phrase
+
+print(match_rule(rules, "do you remember your last birthday"))
+
+def replace_pronouns(message):
+
+    message = message.lower()
+    if 'me' in message:
+        # Replace 'me' with 'you'
+        return re.sub('me', 'you', message)
+    if 'my' in message:
+        # Replace 'my' with 'your'
+        return re.sub('my', 'your', message)
+    if 'your' in message:
+        # Replace 'your' with 'my'
+        return re.sub('your', 'my', message)
+    if 'you' in message:
+        # Replace 'you' with 'me'
+        return re.sub('you', 'me', message)
+
+    return message
+
+send_message("do you remember your last birthday")
+send_message("do you think humans should be worried about AI")
+send_message("I want a robot friend")
+send_message("what if you could be anything you wanted")
